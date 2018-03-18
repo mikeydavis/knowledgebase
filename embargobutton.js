6d331dfc95b7ce5b6aa019291e0de76715e67a7f
mikeydavis <mikiedavis@hotmail.com>
2018-03-18 00:02:52 +0000

    embargobutton.js

---
+++ embargobutton.js
@@ -0,0 +1,31 @@
+var embargoButton = $('#apply-embargo').kendoButton({
+                enable: false,
+                click: function () {
+                    $('#feedback').text('');
+                    embargoDate = kendo.toString(embargoDatePicker.value(), 's');
+                    var modelIds = [];
+                    $('.model-checkbox:checked').each(function () {
+                        modelIds.push($(this).val());
+                    });
+                    if (modelIds.length) {
+                        $.ajax({
+                            url: '@Url.RouteUrl("SetModelEmbargoDates")',
+                            type: 'POST',
+                            data: JSON.stringify({ modelIds: modelIds, embargoUntil: embargoDate }),
+                            contentType: 'application/json; charset=utf-8'
+                        })
+                        .done(function () {
+                            gridModel.nvd.read();
+                            $('#feedback').css('color', 'green').text('Embargo request completed successfully.');
+                            $('#check-all').prop('checked', false);
+                        })
+                        .fail(function () {
+                            $('#feedback').css('color', 'red').text('An error occurred.');
+                        })
+                        .always(function () {
+                        });
+                    } else {
+                        alert('Please select at least one item to embargo.');
+                    }
+                }
+            }).data('kendoButton');
